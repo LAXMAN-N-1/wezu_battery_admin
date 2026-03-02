4b28c6d7c837015357582f57aa7c92680c6f1966
Laxman <laxmanlaxman1629@gmail.com>
2026-03-02 13:37:15 +0530

    Auto-commit: sync core features

---
+++ lib/features/core/api/api_client.dart
@@ -0,0 +1,43 @@
+import 'package:dio/dio.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+
+final apiClientProvider = Provider<ApiClient>((ref) => ApiClient());
+
+class ApiClient {
+  late final Dio _dio;
+  final FlutterSecureStorage storage = const FlutterSecureStorage();
+
+  ApiClient() {
+    const baseUrl = 'https://api.digitaltechs.in';
+    _dio = Dio(BaseOptions(
+      baseUrl: baseUrl,
+      connectTimeout: const Duration(seconds: 30),
+      receiveTimeout: const Duration(seconds: 30),
+      headers: {'Content-Type': 'application/json'},
+    ));
+    _dio.interceptors.add(InterceptorsWrapper(
+      onRequest: (options, handler) async {
+        final token = await storage.read(key: 'admin_token');
+        if (token != null) {
+          options.headers['Authorization'] = 'Bearer $token';
+        }
+        return handler.next(options);
+      },
+    ));
+  }
+
+  Future<Response> get(String path, {Map<String, dynamic>? queryParameters}) =>
+      _dio.get(path, queryParameters: queryParameters);
+
+  Future<Response> post(String path, {dynamic data}) =>
+      _dio.post(path, data: data);
+
+  Future<Response> put(String path, {dynamic data}) =>
+      _dio.put(path, data: data);
+
+  Future<Response> delete(String path) => _dio.delete(path);
+
+  Future<Response> patch(String path, {dynamic data}) =>
+      _dio.patch(path, data: data);
+}
